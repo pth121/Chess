15817bd2ac3c29ba792f126b73ebe60a9e6ad348
hlwjrong <2362376790@qq.com>
2019-04-23 08:38:49 +0800

    2019-4-23

---
+++ src/chineseChess/MoveStep.java
@@ -0,0 +1,18 @@
+package chineseChess;
+
+import java.awt.Point;
+
+/**
+ * ×ß²½Àà
+ * 
+ * @author cnlht
+ * 
+ */
+public class MoveStep implements java.io.Serializable {
+	public Point pStart, pEnd;
+ 
+	public MoveStep(Point p1, Point p2) {
+		pStart = p1;
+		pEnd = p2;
+	}
+}
